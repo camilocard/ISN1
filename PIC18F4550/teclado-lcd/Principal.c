c2dc1325c2e457ae3d81a2e30edeaa5d8ca03dc3
Mario Roberto Samudio Martinez <marioport@marioportpc.ingsamudio.org>
2015-10-01 16:35:00 -0500

    Teclado-lcd
    Programa de clave

--- PIC18F4550/teclado-lcd/Principal.c
+++ PIC18F4550/teclado-lcd/Principal.c
@@ -78,28 +78,36 @@ void teclado()
                   swap(clave[0]);// Cambio de niveles
                   //lcd_gotoxy(6,1);
                   //printf(lcd_putc,"*");
+                  output_high(pin_D7);
                   delay_ms(50);
+                  output_low(pin_D7);
                break;
 
                case 2:
                   clave[1]= datom-0x30; // le quito el ascii
                   clave[0]=clave[0]+clave[1];// El lcd tiene por defecto avanzar
                   //printf(lcd_putc,"*");
+                  output_high(pin_D6);
                   delay_ms(50);
+                  output_low(pin_D6);
                break;
 
                case 3:
                   clave[2]= datom-0x40; // ñe quite el ascii
                   swap(clave[2]);// Cambio de niveles
                   //printf(lcd_putc,"*");
+                  output_high(pin_D5);
                   delay_ms(50);
+                  output_low(pin_D5);
                break;
 
                case 4:
                   clave[3]= datom-0x40; // le quito el ascii
                   clave[2]=clave[2]+clave[3];// El lcd tiene por defecto avanzar
                   //printf(lcd_putc,"*");
+                  output_high(pin_D4);
                   delay_ms(50);
+                  output_low(pin_D4);
                break;
             }
 
@@ -129,330 +137,29 @@ void teclado()
    }
 }
 
-// Métodos Secuencias led
-void secuencia0()
-{
-   contador = 0;
-   while(contador<8)
-   {
-      output_high(pin_A0);
-      delay_ms(25);
-      output_low(pin_A0);
-      delay_ms(25);
-      output_high(pin_A1);
-      delay_ms(25);
-      output_low(pin_A1);
-      delay_ms(25);
-      output_high(pin_A2);
-      delay_ms(25);
-      output_low(pin_A2);
-      delay_ms(25);
-      output_high(pin_A3);
-      delay_ms(25);
-      output_low(pin_A3);
-      delay_ms(25);
-      output_high(pin_A4);
-      delay_ms(25);
-      output_low(pin_A4);
-      delay_ms(25);
-      output_high(pin_A5);
-      delay_ms(25);
-      output_low(pin_A5);
-      delay_ms(25);
-      output_high(pin_B0);
-      delay_ms(25);
-      output_low(pin_B0);
-      delay_ms(25);
-      output_high(pin_B1);
-      delay_ms(25);
-      output_low(pin_B1);
-      delay_ms(25);
-      output_high(pin_B2);
-      delay_ms(25);
-      output_low(pin_B2);
-      delay_ms(25);
-      output_high(pin_B3);
-      delay_ms(25);
-      output_low(pin_B3);
-      delay_ms(25);
-      output_high(pin_B4);
-      delay_ms(25);
-      output_low(pin_B4);
-      delay_ms(25);
-      output_high(pin_B5);
-      delay_ms(25);
-      output_low(pin_B5);
-      delay_ms(25);
-      output_high(pin_B6);
-      delay_ms(25);
-      output_low(pin_B6);
-      delay_ms(25);
-      output_high(pin_B7);
-      delay_ms(25);
-      output_low(pin_B7);
-      delay_ms(25);
-      output_high(pin_C6);
-      delay_ms(25);
-      output_low(pin_C6);
-      delay_ms(25);
-      output_high(pin_C7);
-      delay_ms(25);
-      output_low(pin_C7);
-      delay_ms(25);
-      output_high(pin_D4);
-      delay_ms(25);
-      output_low(pin_D4);
-      delay_ms(25);
-      output_high(pin_D5);
-      delay_ms(25);
-      output_low(pin_D5);
-      delay_ms(25);
-      output_high(pin_D6);
-      delay_ms(25);
-      output_low(pin_D6);
-      delay_ms(25);
-      output_high(pin_D7);
-      delay_ms(25);
-      output_low(pin_D7);
-      delay_ms(25);
-
-      contador+=1;
-   }
-}
-
-void secuencia1()
-{
-   contador = 0;
-   while(contador<2)
-   {
-
-      output_high(pin_A0);
-      output_high(pin_A1);
-      output_high(pin_A2);
-      output_high(pin_A3);
-      output_high(pin_A4);
-      output_high(pin_A5);
-      delay_ms(50);
-      output_low(pin_A0);
-      output_low(pin_A1);
-      output_low(pin_A2);
-      output_low(pin_A3);
-      output_low(pin_A4);
-      output_low(pin_A5);
-      delay_ms(50);
-
-      output_high(pin_B0);
-      output_high(pin_B1);
-      output_high(pin_B2);
-      output_high(pin_B3);
-      output_high(pin_B4);
-      output_high(pin_B5);
-      output_high(pin_B6);
-      output_high(pin_B7);
-      delay_ms(50);
-      output_low(pin_B0);
-      output_low(pin_B1);
-      output_low(pin_B2);
-      output_low(pin_B3);
-      output_low(pin_B4);
-      output_low(pin_B5);
-      output_low(pin_B6);
-      output_low(pin_B7);
-      delay_ms(50);
-      
-      output_high(pin_C6);
-      output_high(pin_C7);
-      delay_ms(50);
-      output_low(pin_C6);
-      output_low(pin_C7);
-      delay_ms(50);
-   
-      output_high(pin_D4);
-      output_high(pin_D5);
-      output_high(pin_D6);
-      output_high(pin_D7);
-      delay_ms(50);
-      output_low(pin_D4);
-      output_low(pin_D5);
-      output_low(pin_D6);
-      output_low(pin_D7);
-      delay_ms(50);
-
-      contador += 1;
-   }
-}
-
-void secuencia2()
-{
-   contador = 0;
-
-   while(contador<2)
-   {
-      output_high(pin_A0);
-      delay_ms(10);
-      output_low(pin_A0);
-      delay_ms(10);
-      output_high(pin_A1);
-      delay_ms(10);
-      output_low(pin_A1);
-      delay_ms(10);
-      output_high(pin_A2);
-      delay_ms(10);
-      output_low(pin_A2);
-      delay_ms(10);
-      output_high(pin_A3);
-      delay_ms(10);
-      output_low(pin_A3);
-      delay_ms(10);
-      output_high(pin_A4);
-      delay_ms(10);
-      output_low(pin_A4);
-      delay_ms(10);
-      output_high(pin_A5);
-      delay_ms(10);
-      output_low(pin_A5);
-      delay_ms(10);
-      output_high(pin_B0);
-      delay_ms(10);
-      output_low(pin_B0);
-      delay_ms(10);
-      output_high(pin_B1);
-      delay_ms(10);
-      output_low(pin_B1);
-      delay_ms(10);
-      output_high(pin_B2);
-      delay_ms(10);
-      output_low(pin_B2);
-      delay_ms(10);
-      output_high(pin_B3);
-      delay_ms(10);
-      output_low(pin_B3);
-      delay_ms(10);
-      output_high(pin_B4);
-      delay_ms(10);
-      output_low(pin_B4);
-      delay_ms(10);
-      output_high(pin_B5);
-      delay_ms(10);
-      output_low(pin_B5);
-      delay_ms(10);
-      output_high(pin_B6);
-      delay_ms(10);
-      output_low(pin_B6);
-      delay_ms(10);
-      output_high(pin_B7);
-      delay_ms(10);
-      output_low(pin_B7);
-      delay_ms(10);
-      output_high(pin_C6);
-      delay_ms(10);
-      output_low(pin_C6);
-      delay_ms(10);
-      output_high(pin_C7);
-      delay_ms(10);
-      output_low(pin_C7);
-      delay_ms(10);
-      output_high(pin_D4);
-      delay_ms(10);
-      output_low(pin_D4);
-      delay_ms(10);
-      output_high(pin_D5);
-      delay_ms(10);
-      output_low(pin_D5);
-      delay_ms(10);
-      output_high(pin_D6);
-      delay_ms(10);
-      output_low(pin_D6);
-      delay_ms(10);
-      output_high(pin_D7);
-      delay_ms(10);
-      output_low(pin_D7);
-      delay_ms(10);
-
-      contador +=1;
-
-   }
-}
-
-void secuencia3()
-{
-   output_high(pin_E0);
-   delay_ms(25);
-   output_low(pin_E0);
-   delay_ms(25);
-}
-
-void secuencia4()
-{
-   contador = 0;
-   while(contador<2)
-   {
-      output_high(pin_A1);
-      delay_ms(10);
-      output_low(pin_A1);
-      delay_ms(10);
-      output_high(pin_A3);
-      delay_ms(10);
-      output_low(pin_A3);
-      delay_ms(10);
-      output_high(pin_A5);
-      delay_ms(10);
-      output_low(pin_A5);
-      delay_ms(10);
-      output_high(pin_B1);
-      delay_ms(10);
-      output_low(pin_B1);
-      delay_ms(10);
-      output_high(pin_B3);
-      delay_ms(10);
-      output_low(pin_B3);
-      delay_ms(10);
-      output_high(pin_B5);
-      delay_ms(10);
-      output_low(pin_B5);
-      delay_ms(10);
-      output_high(pin_B7);
-      delay_ms(10);
-      output_low(pin_B7);
-      delay_ms(10);
-      output_high(pin_C7);
-      delay_ms(10);
-      output_low(pin_C7);
-      delay_ms(10);
-      output_high(pin_D5);
-      delay_ms(10);
-      output_low(pin_D5);
-      delay_ms(10);
-      output_high(pin_D7);
-      delay_ms(10);
-      output_low(pin_D7);
-      delay_ms(10);
-
-      contador += 1;
-   }
-}
 
 // Método principal
 void main()
 {
    set_tris_A(0x00);/* (0000 0000)
-      RA0   LED1   PIN   02
-      RA1   LED2   PIN   03
-      RA2   LED3   PIN   04
-      RA3   LED4   PIN   05
-      RA4   LED5   PIN   06
-      RA5   LED6   PIN   07
+      RA0   -----   PIN   02
+      RA1   -----   PIN   03
+      RA2   -----   PIN   04
+      RA3   -----   PIN   05
+      RA4   -----   PIN   06
+      RA5   -----   PIN   07
    */
 
-   set_tris_B(0x00);/* (0000 0000)
-      RB0   LED7    PIN   40
-      RB1   LED8    PIN   39
-      RB2   LED9    PIN   38
-      RB3   LED10   PIN   37
-      RB4   LED11   PIN   36
-      RB5   LED12   PIN   35
-      RB6   LED13   PIN   34
-      RB7   LED14   PIN   33
+   // Configuración de pines para el teclado
+   set_tris_B(0x00);/* (0000 1111)
+      RB0   COL0    PIN   40
+      RB1   COL1    PIN   39
+      RB2   COL2    PIN   38
+      RB3   COL3    PIN   37
+      RB4   Fila0   PIN   36
+      RB5   Fila1   PIN   35
+      RB6   Fila2   PIN   34
+      RB7   Fila3   PIN   33
    */
 
    set_tris_C(0x00);/* (00-- -xxx)
@@ -460,20 +167,19 @@ void main()
       RC0   -----   PIN   15
       RC1   -----   PIN   16   
       RC2   -----   PIN   17
-      RC6   LED15   PIN   25
-      RC7   LED16   PIN   26
+      RC6   LED01   PIN   25
+      RC7   LED02   PIN   26
    */
 
-   // Configuración de Leds 17 hasta 20 y Pulsadores 1 y 2   
-   set_tris_D(0x00);/* (0000 xx00)  Para el teclado (1111 0000)set_tris_D(0xF0)
+   set_tris_D(0x00);/* (0000 xx00)
       RD0   SW01    PIN   19 --->Pulsador 1
       RD1   SW02    PIN   20 --->Pulsador 2
       RD2   -----   PIN   21
       RD3   -----   PIN   22
-      RD4   LED17   PIN   27
-      RD5   LED18   PIN   28
-      RD6   LED19   PIN   29
-      RD7   LED20   PIN   30
+      RD4   LED03   PIN   27
+      RD5   LED04   PIN   28
+      RD6   LED05   PIN   29
+      RD7   LED06   PIN   30
    */
 
    set_tris_E(0x00);/* bits(---- xxxx)
@@ -505,22 +211,17 @@ void main()
      el pulsador esté presionado. */
    output_float(SW01);
    output_float(SW02);
-   while(TRUE)
-   {
-      if (input_state(SW01) == 1 && input_state(SW02) == 1)
-      {
-         secuencia1();
-      }else if (input_state(SW01) == 0 && input_state(SW02) == 1)
-      {
-         secuencia2();
-      }else if (input_state(SW01) == 1 && input_state(SW02) == 0)
-      {
-         secuencia4();
-      }else
-      {
-      }
-   }
 
+
+   //inicializando
+   while(contador <= 2)
+   {
+      output_high(pin_C6);
+      delay_ms(1000):
+      output_low(pin_C6);
+      delay_ms(1000);
+      contador += 1;
+   }
 
    // Contraseña definida en: *8BC
    //    clave[0] = 0x2a;// ===> *
@@ -528,14 +229,18 @@ void main()
    //    clave[2] = 0x42;// ===> B
    //    clave[3] = 0x43;// ===> C
 
-   // teclado();
-   // if ((clave[0]== 0xa8) & (clave[2] == 0x23))  
-   // {
-   //     secuencia1();
-   // }else
-   // {
-   //    /* codigo */
-   // }
+   teclado();
+   if ((clave[0]== 0xa8) & (clave[2] == 0x23))  
+   {
+       output_high(pin_C7);
+       delay_ms(2000);
+       output_low(pin_C7);
+   }else
+   {
+      output_high(pin_C6);
+      delay_ms(2000);
+      output_low(pin_C6);
+   }
 
    //vuelva a inicio y vuelva y arranque
    reset_cpu();
